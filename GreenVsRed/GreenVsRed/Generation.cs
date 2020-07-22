b872ae2a8d54c491636c72ad3a9055a8d789449c
Svetoslav Krastev <svetoslav.tkrastev@gmail.com>
2020-07-22 13:10:00 +0300

    added Generation class

---
+++ GreenVsRed/GreenVsRed/Generation.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace GreenVsRed
+{
+    public class Generation
+    {
+        private int x;
+        private int y;
+        private int[][] grid;
+        private int x1;
+        private int y1;
+
+        public Generation(int x, int y, int[][] genZero, int x1, int y1)
+        {
+            this.x = x;
+            this.y = y;
+            this.grid = genZero;
+            this.x1 = x1;
+            this.y1 = y1;
+        }
+    }
+}
---
+++ GreenVsRed/GreenVsRed/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GreenVsRed
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] firstLine = Console.ReadLine().Split();
+            int x = int.Parse(firstLine[0]);
+            int y = int.Parse(firstLine[1]);
+        }
+    }
+}
